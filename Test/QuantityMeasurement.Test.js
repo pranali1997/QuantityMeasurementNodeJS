063b1b761c43574ff9db72fe031de8549cf8f856
pranali1997 <pranali.lembhe77@gmail.com>
2020-02-14 17:16:33 +0530

    Refactored : code for inch to feet conversion

--- Test/QuantityMeasurement.Test.js
+++ Test/QuantityMeasurement.Test.js
@@ -3,15 +3,18 @@ const quantityMeasure=require('../app/QuantityMeasurement.js');
 
 describe('QuantityMeasurement',function(){
     it('0 feet should be equal to 0 feet',function(){
-        assert.equal(quantityMeasure.unitMeasurement("feet",0),0);
+        assert.equal(quantityMeasure.unitMeasurement("feet","inch",0),0);
     }),
     it('0 inch should be equal to 0 inch',function(){
-        assert.equal(quantityMeasure.unitMeasurement("inch",0),0);
+        assert.equal(0,quantityMeasure.unitMeasurement("inch","inch",0));
     }),
     it('1 feet should be equal to 12 inch',function(){
-        assert.equal(quantityMeasure.unitMeasurement("feet",1),12);
+        assert.equal(12,quantityMeasure.unitMeasurement("feet","inch",1),12);
     })
     it('feet value should be a number',function(){
-        assert.typeOf(quantityMeasure.unitMeasurement("feet",1),'Number');
+        assert.typeOf(quantityMeasure.unitMeasurement("feet","inch",1),'Number');
+    })
+    it('12 inch should be a equal to 1 feet',function(){
+        assert.equal(quantityMeasure.unitMeasurement("inch","feet",12),1);
     })
 })
